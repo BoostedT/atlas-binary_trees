77910f93323cbac7ea44695dbfd1fcd289d29c08
BoostedT <tyler.whitchurch@atlasschool.com>
2024-08-12 13:03:39 -0500

    added new code to 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,11 +9,10 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-size_t size_l;
-size_t size_r;
+size_t size = 0;
 if (!tree)
 return (0);
-size_l = tree->left ? 1 + binary_tree_size(tree->left) : 0;
-size_r = tree->right ? 1 + binary_tree_size(tree->right) : 0;
-return (size_l + size_r);
+size += binary_tree_size(tree->left);
+size += binary_tree_size(tree->right);
+return (size + 1);
 }
