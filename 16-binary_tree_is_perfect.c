0469b0a494de66157bca0c852e714303538ba5bf
BoostedT <tyler.whitchurch@atlasschool.com>
2024-08-12 20:33:38 -0500

    FIX

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,13 +7,10 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-if (tree == NULL)
+if (!tree)
 return (0);
-if (tree->left == NULL && tree->right == NULL)
+if (binary_tree_is_full(tree) && !binary_tree_balance(tree))
 return (1);
-if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 return (0);
-if (tree->left && tree->right)
-return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right);
-return (0));
 }
+
