a43999e911072746e72517f87833814fc8e85ad7
BoostedT <tyler.whitchurch@atlasschool.com>
2024-08-12 20:28:34 -0500

    betty style for 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,6 +12,6 @@ return (0);
 if (tree->left == NULL && tree->right == NULL)
 return (1);
 if (tree->left && tree->right)
-return (binary_tree_is_full(tree->left)) && binary_tree_is_full(tree->right);
-return (0);
+return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+return (0));
 }
