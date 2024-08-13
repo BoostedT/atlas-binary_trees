199835794d42fd9ec26cd9df2d33af6c0a0f23ca
BoostedT <tyler.whitchurch@atlasschool.com>
2024-08-12 20:32:28 -0500

    betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,5 +15,5 @@ if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 return (0);
 if (tree->left && tree->right)
 return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right);
-return (0);
+return (0));
 }
