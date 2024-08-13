15a7d8a10bc40e9d7cb85a258b8f843e75ffe0f7
BoostedT <tyler.whitchurch@atlasschool.com>
2024-08-12 20:44:14 -0500

    1

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,49 +1,51 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree to check
- * Return: 1 if perfect, 0 if not perfect or tree is NULL
+ * tree_is_perfect - function that says if a tree is perfect or not
+ * it has to be the same quantity of levels in left as right, and also
+ * each node has to have 2 nodes or none
+ * @tree: tree to check
+ * Return: 0 if is not a perfect or other number that is the level of height
+ */
+int tree_is_perfect(const binary_tree_t *tree)
+{
+int l = 0, r = 0;
+if (tree->left && tree->right)
+{
+l = 1 + tree_is_perfect(tree->left);
+r = 1 + tree_is_perfect(tree->right);
+if (r == l && r != 0 && l != 0)
+return (r);
+return (0);
+}
+else if (!tree->left && !tree->right)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+/**
+ * binary_tree_is_perfect - perfect or not a tree
+ * @tree: tree to check
+ * Return: 1 is it is or 0 if not
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-if (!tree)
+int result = 0;
+if (tree == NULL)
+{
 return (0);
-if (binary_tree_leaves(tree) == binary_tree_nodes(tree) + 1)
+}
+else
+{
+result = tree_is_perfect(tree);
+if (result != 0)
+{
 return (1);
+}
 return (0);
 }
-
-/**
- * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: pointer to the root node of the tree to count the number of leaves
- * Return: number of leaves in a binary tree
- */
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-size_t leaves = 0;
-if (tree == NULL)
-return (0);
-if (tree->left == NULL && tree->right == NULL)
-leaves++;
-leaves += binary_tree_leaves(tree->left);
-leaves += binary_tree_leaves(tree->right);
-return (leaves);
-}
-
-/**
- * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
- * @tree: pointer to the root node of the tree to count the number of nodes
- * Return: number of nodes with at least 1 child in a binary tree
- */
-size_t binary_tree_nodes(const binary_tree_t *tree)
-{
-size_t nodes = 0;
-if (tree == NULL)
-return (0);
-if (tree->left != NULL || tree->right != NULL)
-nodes++;
-nodes += binary_tree_nodes(tree->left);
-nodes += binary_tree_nodes(tree->right);
-return (nodes);
 }
