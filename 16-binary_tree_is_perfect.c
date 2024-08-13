d16f12b7defb89ca38a61348574feed7ddc25f3f
BoostedT <tyler.whitchurch@atlasschool.com>
2024-08-12 20:35:34 -0500

    actually fixed it

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,8 +9,41 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 if (!tree)
 return (0);
-if (binary_tree_is_full(tree) && !binary_tree_balance(tree))
+if (binary_tree_leaves(tree) == binary_tree_nodes(tree) + 1)
 return (1);
 return (0);
 }
 
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: number of leaves in a binary tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+size_t leaves = 0;
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+leaves++;
+leaves += binary_tree_leaves(tree->left);
+leaves += binary_tree_leaves(tree->right);
+return (leaves);
+}
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of nodes
+ * Return: number of nodes with at least 1 child in a binary tree
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+size_t nodes = 0;
+if (tree == NULL)
+return (0);
+if (tree->left != NULL || tree->right != NULL)
+nodes++;
+nodes += binary_tree_nodes(tree->left);
+nodes += binary_tree_nodes(tree->right);
+return (nodes);
+}
